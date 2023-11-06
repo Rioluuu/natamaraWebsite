9242d522fe7720055552663da53a11631e4f55be
Maurice Schulz <116212130+Bijohloge@users.noreply.github.com>
2023-11-06 22:42:49 +0000

    Katalog hinzugefügt

--- src/Catalogue.vue
+++ src/Catalogue.vue
@@ -1,3 +1,43 @@
+<script>
+    import CatalogueItem from './components/CatalogueItem.vue'
+
+    export default {
+        components: {
+        CatalogueItem, 
+        },
+        data() {
+    return {
+      products: [
+        {name: "Test1", description: "awdaew", imageUrl: "../assets/logo_small.svg"},
+        {name: "Test2", description: "awdawdaew", imageUrl: "../assets/logo_small.svg"},
+        {name: "Test3", description: "awdaawadaew", imageUrl: "../assets/logo_small.svg"},
+      ]
+    };
+  }
+};
+</script>
+
+
+
 <template>
     <h1>Katalog</h1>
+    <div id="wrapper">
+        <CatalogueItem  v-for="x in products"
+      :name ="x.name"
+      :description="x.description"
+      :imageUrl="x.imageUrl" />
+    </div>
+    
 </template>
+
+<style scoped>
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    flex-basis: 120px;
+    margin: 10px;
+    padding: 10px;
+  }
+</style> 
---
+++ src/components/CatalogueItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="shop-item">
+      <img src="../assets/logo_small.svg" class="item-image">
+      <h2 class="item-name">{{ name }}</h2>
+      <p class="item-description">{{ description }}</p>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+        props: {
+            name: String,
+            description: String,
+            imageUrl: String,
+        }
+    }
+  </script>
+  
+  <style scoped>
+  .shop-item {
+    border: 1px solid #ccc;
+    padding: 16px;
+    text-align: center;
+  }
+  
+  .item-image {
+    height: auto;
+    width: 30rem;
+  }
+  
+  .item-name {
+    font-size: 1.5rem;
+    margin: 8px 0;
+  }
+  
+  .item-description {
+    margin: 8px 0;
+  }
+  </style>
+  
